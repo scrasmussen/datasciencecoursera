e44f7b2624f0cc2198b27b80b79c8f914154832a
Soren Rasmussen <s.c.rasmussen@gmail.com>
2016-03-04 11:17:39 -0800

    R script to creat plot of Baltimore's yearly total PM2.5 emissions

---
+++ course_project_2/plot2.R
@@ -0,0 +1,17 @@
+# Read in data
+nei <- readRDS("summarySCC_PM25.rds")
+
+png(filename = "plot2.png", width = 480, height = 480)
+
+# Get sum of emissions for each year
+# Baltimore's fips number
+baltimore <- 24510
+baltimore_data <- nei[nei$fips==baltimore,]
+yearly_sum <- aggregate(Emissions ~ year, baltimore_data, sum)
+colnames(yearly_sum)[2] <- "Total Emissions Per Year"
+plot(yearly_sum, type="o", axes=FALSE)
+title(main="Baltimore PM2.5 Output")
+axis(1, at=yearly_sum$year)
+axis(2)
+box()
+dev.off()
