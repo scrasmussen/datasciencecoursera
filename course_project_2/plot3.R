0a0270e54fe544481a099e1667ef1581cff9c766
Soren Rasmussen <s.c.rasmussen@gmail.com>
2016-03-04 15:46:33 -0800

    Update titles

--- course_project_2/plot3.R
+++ course_project_2/plot3.R
@@ -11,5 +11,5 @@ baltimore <- 24510
 baltimore_data <- nei[nei$fips==baltimore,]
 baltimore_type <- aggregate(baltimore_data$Emissions, list(year=baltimore_data$year, type=baltimore_data$type), sum)
 
-ggplot(baltimore_type, aes(x=year, y=x, group=type, color=type)) + geom_line() + geom_point() + ylab("PM2.5 Emissions") + ggtitle("Baltimore PM2.5 Emissions")
+ggplot(baltimore_type, aes(x=year, y=x, group=type, color=type)) + geom_line() + geom_point() + ylab("PM2.5 Levels") + ggtitle("Baltimore PM2.5 Emissions")
 dev.off()
